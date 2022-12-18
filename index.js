1d6dc63b515d095f09a251ba7dbd955ee79e1a92
Atul kumar jha <107975204+Atuljha27@users.noreply.github.com>
2022-12-18 14:06:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let A = document.getElementById("login");
+let B = document.getElementById("register");
+let C = document.getElementById("btn");
+
+const register = function () {
+  A.style.left = "-400px";
+  B.style.left = "50px";
+  C.style.left = " 110px";
+};
+
+const login = function () {
+  A.style.left = "50px";
+  B.style.left = "450px";
+  C.style.left = " 0";
+};
